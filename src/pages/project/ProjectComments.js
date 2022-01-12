c5a2effe5e139ba389a9a0181800a7480d575901
sutejakw <sutejakawinanga29@gmail.com>
2022-01-12 16:03:56 +0800

    add comments

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import { useAuthContext } from "../../hooks/useAuthContext";
 import { timestamp } from "../../firebase/config";
+import { useFirestore } from "../../hooks/useFirestore";
+import Avatar from "../../components/avatar/Avatar";
 
-export default function ProjectComments() {
+export default function ProjectComments({ project }) {
   const [newComment, setNewComment] = useState("");
   const { user } = useAuthContext();
+  const { updateDocument, response } = useFirestore("projects");
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
     const commentToAdd = {
@@ -16,12 +19,36 @@ export default function ProjectComments() {
       createdAt: timestamp.fromDate(new Date()),
       id: Math.random(),
     };
-    console.log(commentToAdd);
+
+    updateDocument(project.id, {
+      comments: [...project.comments, commentToAdd],
+    });
+
+    if (!response.error) {
+      setNewComment("");
+    }
   };
   return (
     <div className="project-comments">
       <h4>Project Comments</h4>
 
+      <ul>
+        {project.comments.length > 0 &&
+          project.comments.map((comment) => (
+            <li key={comment.id}>
+              <div className="comment-author">
+                <Avatar src={comment.photoURL} />
+                <p>{comment.displayName}</p>
+              </div>
+              <div className="comment-date">
+                <p>date here</p>
+              </div>
+              <div className="comment-content">
+                <p>{comment.content}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
       <form className="add-comment" onSubmit={handleSubmit}>
         <label>
           <span>Add new comment:</span>
